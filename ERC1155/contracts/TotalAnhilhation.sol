89c6fcce067236f7e04283d141b85c3df188f2c1
rafa <rafacrespo2002@hotmail.com>
2022-11-27 23:26:33 +0100

    json

--- ERC1155/contracts/TotalAnhilhation.sol
+++ ERC1155/contracts/TotalAnhilhation.sol
@@ -22,51 +22,13 @@ contract AgeOfEmpires is
         0x85a1b7ad92c3d354582F9F858E5fF7a6b7A615ce;
 
     // Modifiers
-    modifier enoughResources(uint256 id) {
+    modifier enoughResources(string id) {
         if (id == 4) {
             require(
                 balanceOf(msg.sender, 0) >= 25,
                 "Not enough wood to build a house"
             );
-        } else if (id == 5) {
-            require(
-                balanceOf(msg.sender, 0) >= 100,
-                "Not enough wood to build a fortress"
-            );
-            require(
-                balanceOf(msg.sender, 1) >= 150,
-                "Not enough stone to build a fortress"
-            );
-        } else if (id == 6) {
-            require(
-                balanceOf(msg.sender, 0) >= 120,
-                "Not enough wood to build a cathedral"
-            );
-            require(
-                balanceOf(msg.sender, 1) >= 180,
-                "Not enough stone to build a cathedral"
-            );
-            require(
-                balanceOf(msg.sender, 2) >= 50,
-                "Not enough copper to build a cathedral"
-            );
-        } else if (id == 7) {
-            require(
-                balanceOf(msg.sender, 0) >= 140,
-                "Not enough wood to build a sanctuary"
-            );
-            require(
-                balanceOf(msg.sender, 1) >= 200,
-                "Not enough stone to build a sanctuary"
-            );
-            require(
-                balanceOf(msg.sender, 2) >= 100,
-                "Not enough copper to build a sanctuary"
-            );
-            require(
-                balanceOf(msg.sender, 3) >= 50,
-                "Not enough gold to build a sanctuary"
-            );
+        
         }
         _;
     }
@@ -76,10 +38,13 @@ contract AgeOfEmpires is
             /* nft storage
             "ipfs://bafybeiatlk3xsofqa476cfkmohohmf2d5m7tbpibkp5yape2jgmic6enzy/{id}" */
             // pinata
-            "https://ipfs.io/ipfs/QmVMf4t7d5DjfgdB9pwL6kBWowFxN7jaVxSxQRewSWkrgy/{id}"
+            "ipfs://bafybeiew43lpws3zurlyogl724sczh2d36ynjct43b4r7mzg5nrbtsq25m/{id}"
         )
     {
         name = _name;
+        _mint(msg.sender, "1", 10000, "");
+        _mint(msg.sender, "2", 10000, "");
+        _mint(msg.sender, "0", 1, "");
     }
 
     function pause() public onlyOwner {
